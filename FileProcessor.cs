165c8e462e426bce55fa865e1241dbf7ac3ffa8b
Yury Fedorov <yury.fedorov@gmail.com>
2019-03-22 16:22:16 +0100

    NetRefactorer.Core files

---
+++ FileProcessor.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace NetRefactorer
+{
+    public class FileProcessor
+    {
+        // asynchronous processing
+        private readonly Func<string,Task> _processFile;
+
+        public FileProcessor(Func<string,Task> processFile)
+        {
+            _processFile = processFile;
+        }
+
+        public async Task ProcessFile(string filename)
+        {
+            await _processFile(filename);
+        }
+
+        public async Task Process(string directory)
+        {
+            var info = new DirectoryInfo(directory);
+            var tasks = new List<Task>();
+            foreach (var entry in info.EnumerateFileSystemInfos())
+            {
+                var processor = entry.Attributes.HasFlag(FileAttributes.Directory) ? (Func<string, Task>) Process : ProcessFile;
+                tasks.Add(processor(entry.FullName));
+            }
+            await Task.WhenAll(tasks);
+        }
+    }
+}
---
+++ Refactorer.cs
@@ -0,0 +1,65 @@
+﻿using NetRefactorer.Interface;
+using System;
+using System.Collections.Generic;
+using System.Composition.Hosting;
+using System.IO;
+using System.Linq;
+using System.Runtime.Loader;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NetRefactorer
+{
+    // composer based on MEF (Managed Extensibility Framework)
+    public class Refactorer
+    {
+        private IEnumerable<IRefactorer> refactoringModuleList;
+
+        public Refactorer()
+        {
+            // https://blog.softwarepotential.com/porting-to-net-standard-2-0-part-2-porting-mef-1-0-to-mef-2-0-on-net-core/
+            var directory = AppDomain.CurrentDomain.BaseDirectory;
+            var assemblies = Directory.GetFiles(directory, "*.dll")
+                .Select(AssemblyLoadContext.Default.LoadFromAssemblyPath);
+
+            var configuration = new ContainerConfiguration().WithAssemblies(assemblies);
+            var container = configuration.CreateContainer();
+
+            refactoringModuleList = container.GetExports<IRefactorer>();
+        }
+
+        public static async Task RefactorDirectory(string root)
+        {
+            var refactorer = new Refactorer();
+            var fileProcessor = new FileProcessor(refactorer.RefactorFile);
+            await fileProcessor.Process(root);
+        }
+
+        public async Task RefactorFile(string file)
+        {
+            var fileText = File.ReadAllText(file);
+            var refactored = Refactor(file, fileText);
+            if (fileText == refactored)
+            {
+                refactored = null;
+            }
+
+            if (refactored!=null)
+            {
+                File.WriteAllText(file, refactored);
+            }
+        }
+
+        public string Refactor(string filename, string code)
+        {
+            foreach (var refactoringModule in refactoringModuleList)
+            {
+                if (refactoringModule.ToRefactor(filename))
+                {
+                    code = refactoringModule.Refactor(code);
+                }
+            }
+            return code;
+        }
+    }
+}
